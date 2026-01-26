c9a09d07077785ddedf6a1e8c472b5372f115326
Brian Ademba <brian.ademba@external.toyota-europe.com>
2026-01-26 22:38:44 +0300

    implemented supplier creation

---
+++ src/main/java/com/kendirita/travel_tour/repository/SupplierRepository.java
@@ -0,0 +1,10 @@
+package com.kendirita.travel_tour.repository;
+
+import com.kendirita.travel_tour.entity.Suppliers;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository  extends JpaRepository<Suppliers,String> {
+    boolean existsByEmail(String email);
+}
